09003697b2a19c1b792e824e79146534945c3ace
Athipat <athipatpingwong@gmail.com>
2024-02-22 01:55:00 +0700

    Update update by id

--- routes/users.js
+++ routes/users.js
@@ -53,7 +53,7 @@ router
     .get(async(req, res) => {
         try {
             const {id} = req.params
-            const user = await User.findById(id)
+            const user = await User.findByIdAndUpdate(id)
             res.status(200).json(user)
         } catch(error) {
             res.status(500).json({message: error.message})
